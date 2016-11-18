07baf12fa94a73b8eb1b850efcd6b348a4eb1386
Brandon Straley <straleyb@onid.oregonstate.edu>
2016-11-18 09:14:09 -0800

    Refactor overriden method. Use ActiveFedoraBase find method.

--- app/search_builders/curation_concerns/filter_suppressed_with_roles.rb
+++ app/search_builders/curation_concerns/filter_suppressed_with_roles.rb
@@ -3,21 +3,21 @@ module CurationConcerns
     extend CurationConcerns::FilterSuppressed
 
     def only_active_works(solr_parameters)
-      if user_work_permissions.empty?
-        super unless current_ability.current_user.admin?
+      super if user_work_permissions.empty?
+    end
+
+    private
+
+      def current_work
+        ActiveFedora::Base.find(blacklight_params[:id])
       end
-    end
 
-    def current_work
-      GenericWork.find(blacklight_params[:id])
-    end
+      def user_work_permissions
+        permission_query.scope_permitted_workflow_actions_available_for_current_state(:user => current_ability.current_user, :entity => current_work.to_sipity_entity)
+      end
 
-    def user_work_permissions
-      permission_query.scope_permitted_workflow_actions_available_for_current_state(:user => current_ability.current_user, :entity => current_work.to_sipity_entity)
-    end
-
-    def permission_query
-      CurationConcerns::Workflow::PermissionQuery
-    end
+      def permission_query
+        CurationConcerns::Workflow::PermissionQuery
+      end
   end
 end
